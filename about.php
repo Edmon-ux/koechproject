dc0fe1e9d26f51b7b02a614a810917bb2900a393
Gamer Snave <snaveqiptoo@gmail.com>
2024-03-19 09:32:29 +0300

    Initial Major  commit

---
+++ about.php
@@ -0,0 +1,48 @@
+ <!-- Masthead-->
+        <header class="masthead">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-10 align-self-end mb-4" style="background: #0000002e;">
+                    	 <h1 class="text-uppercase text-white font-weight-bold">About Us</h1>
+                        <hr class="divider my-4" />
+                    </div>
+                    
+                </div>
+            </div>
+        </header>
+
+    <section class="page-section">
+        <div class="container">
+
+        <h1 style="text-align: center; font-size: 28px;">ABOUT US</h1>
+
+<p style="text-align: justify; font-family: 'Open Sans', Arial, sans-serif;">
+  At HealthLine, we believe that healthcare shouldn't be a hassle. That's why we created a platform designed to make scheduling appointments and managing your health a breeze.
+</p>
+
+<h2>Our Mission</h2>
+
+<p style="text-align: justify; font-family: 'Open Sans', Arial, sans-serif;">
+  Our mission is to empower patients by providing a simple, secure, and efficient way to connect with healthcare providers. We strive to streamline the appointment booking process, improve communication between patients and doctors, and ultimately, enhance the overall patient experience.
+</p>
+
+
+<ul>
+  <li> <strong>Find the Right Doctor:</strong>  Search for doctors by location, insurance, and specialty. Read detailed profiles, including qualifications, experience, and patient reviews. </li>
+  <li> <strong>Schedule Appointments 24/7:</strong>  Browse doctor availability and book appointments directly through our platform, anytime, anywhere. No more hold music or inconvenient phone tag!</li>
+</ul>
+
+<h2>Your Health is Our Priority</h2>
+
+<p style="text-align: justify; font-family: 'Open Sans', Arial, sans-serif;">
+  HealthLine values your privacy and security. We utilize industry-standard data encryption to keep your information safe. Additionally, we help simplify the insurance verification process, so you can focus on your health, not the paperwork. 
+</p>
+
+<h2>Join the HealthLine Community</h2>
+
+<p style="text-align: justify; font-family: 'Open Sans', Arial, sans-serif;">
+  We're committed to continuous improvement and innovation. We value your feedback and suggestions. Let's work together to build a better healthcare experience for everyone.
+</p>
+         
+        </div>
+        </section>
